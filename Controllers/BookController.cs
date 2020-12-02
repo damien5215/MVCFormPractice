efb768370ac9f81b299edfde75f385d666458663
damien5215 <37832934+damien5215@users.noreply.github.com>
2020-12-02 14:13:50 +0000

    Added BookRepository
    Added BookRepository and BookContoller.

---
+++ Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using MVCFormPractice.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFormPractice.Controllers
+{
+    public class BookController : Controller
+    {
+        private BookRepository _bookRepository = null;
+
+        public BookController() 
+        {
+            _bookRepository = new BookRepository();
+        }
+
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/BookRepository.cs
@@ -0,0 +1,30 @@
+﻿using MVCFormPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCFormPractice.Data
+{
+    public class BookRepository
+    {
+        private static Book[] _book = new Book[]
+        {
+            new Book()
+            {
+                Id = 1,
+                Title = "Nausea",
+                Author = "Jean Paul Sartre",
+                Description = "fddf ddfd"
+            },
+
+            new Book()
+            {
+                Id = 2,
+                Title = "The Bell Jar",
+                Author = "Sylvia Plath",
+                Description = "fdd fd dfs ddd dds sdd"
+            }
+        };
+    }
+}
