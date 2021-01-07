f809b24ea41ddecf4aa92ecdc8f4d0b87d475dcf
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-01-07 23:22:17 +0000

    Added Enum "FictionEnum"

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,11 +8,11 @@ namespace MVCFormPractice.Models
 {
     public class Book
     {
-        //public enum FictionEnum
-        //{
-        //    Fiction,
-        //    NonFiction
-        //}
+        public enum FictionEnum
+        {
+            Fiction,
+            NonFiction
+        }
 
         // default constructor
         public Book() 
@@ -20,14 +20,14 @@ namespace MVCFormPractice.Models
         }
 
         public Book(int id, Genre.GenreType genreType, string title, string author,
-            string description, bool inStock, int bookID)
+            string description, bool inStock, FictionEnum fiction, int bookID)
         {
             Id = id;
             GenreId = (int)genreType;     
             Title = title;
             Author = author;
             Description = description;
-            //Fiction = fiction;
+            Fiction = fiction;
             InStock = inStock;
             BookID = bookID;
         }
@@ -40,8 +40,7 @@ namespace MVCFormPractice.Models
         public string Title { get; set; }
         public string Author { get; set; }
         public string Description { get; set; }
-        //public FictionEnum Fiction { get; set; }
-
+        public FictionEnum Fiction { get; set; }   // dsssssssssssd
         public bool InStock { get; set; }
         public int BookID { get; set; }
 
