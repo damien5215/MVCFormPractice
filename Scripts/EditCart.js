be5fc1520dff4edaf6a96e4ceb70e82bf04a3617
damien5215 <37832934+damien5215@users.noreply.github.com>
2021-07-02 09:16:12 +0100

    Added Script (Edit Cart)

---
+++ Scripts/EditCart.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+    $(".btnAdd").on("click", function () {
+
+        $("#message1").text("test" + this.id);
+
+        $.ajax({
+            url: "/Book/AddToCart2/" + this.id,
+            type: "GET"
+            //data: { year: ((val * 1) + 1) }
+        }).done(function () {
+
+            $.ajax({
+                url: "/Book/_Cart",
+                type: "GET"
+                //data: { year: ((val * 1) + 1) }
+            }).done(function (partialViewResult) {
+                $("#refTable").html(partialViewResult);
+            });
+        });
+    });
+
+
+    $(".btnDelete").on("click", function () {
+
+        $.ajax({
+            url: "/Book/RemoveFromCart/" + this.id,
+            type: "GET"
+        }).done(function () {
+
+            $.ajax({
+                url: "/Book/_Cart",
+                type: "GET"
+            })
+                .done(function (partialViewResult) {
+                    $("#refTable").html(partialViewResult);
+                });
+        });
+    });
+
+});
